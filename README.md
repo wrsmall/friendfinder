b7b0bbc3f9ba392da569d51443769ff13c86b014
wrsmall <william.r.small89@gmail.com>
2019-08-14 17:24:24 -0400

    got the server and the html done

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # friendfinder
-This app helps people find like friends
+In this activity, I built a compatibility-based "FriendFinder" application -- basically a dating app. This full-stack site takes in results from your users' surveys, then compares their answers with those from other users. The app then displays the name and picture of the user with the best overall match.
+
+I used Express to handle routing,  deployed the app to Heroku so other users can fill it out.
---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require("express");
+
+var app = express();
+
+var PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//////////////////////////    Routers
+require("./routesHTML")(app);
+require("./routesAPI")(app);
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
