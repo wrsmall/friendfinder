b767269eb163410fd9dcc6a3bfce83ac8ebf5e41
wrsmall <william.r.small89@gmail.com>
2019-08-20 18:40:31 -0400

    fixed a bug

--- routes/routesAPI.js
+++ routes/routesAPI.js
@@ -15,7 +15,7 @@ module.exports = function (app) {
 		var matchImage = '';
 		var totalDifference = 50000;
 		for (var i = 0; i < people.length; i++) {
-			var diff=parseInt();
+			var diff=0;
 			for (var x = 0; x < thisuser.length; j++) {
 				diff += Math.abs(people[i].scores[j] - thisuser[x]);
 			}
