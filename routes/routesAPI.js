46bd5c5563cc97bfc189a7351ffecc473102df86
wrsmall <william.r.small89@gmail.com>
2019-08-20 21:12:11 -0400

    fixed my logic

--- routes/routesAPI.js
+++ routes/routesAPI.js
@@ -1,38 +1,41 @@
 var people = require('../data/people');
 
 module.exports = function (app) {
-   
-    app.get("/api/tables", function(req, res) {
-        res.json(people);
-      });
+
+	app.get("/api/tables", function (req, res) {
+		res.json(people);
+	});
 
 
-    app.post("/api/member", function (req, res) {
-        var thisuser=req.body;
-        var uservaules=thisuser.value
-        console.log(uservaules);
-        var matchName = '';
+	app.post("/api/member", function (req, res) {
+		var thisuser = req.body;
+		var uservaules = thisuser.value
+		console.log(uservaules);
+		var matchName = '';
 		var matchImage = '';
-		var totalDifference = 50000;
+		var alldiff = [];
 		for (var i = 0; i < people.length; i++) {
-			var diff=0;
-			for (var x = 0; x < thisuser.length; x++) {
-				diff += Math.abs(people[i].value[i] - thisuser[x]);
+			var diff = 0;
+			for (var x = 0; x < thisuser.value.length; x++) {
+				diff += Math.abs(people[i].value[x] - thisuser.value[x]);
 			}
 			console.log('diff = ' + diff);
-			if (diff < totalDifference) {
-				
-				totalDifference = diff;
-				matchName = people[i].name;
-				matchImage = people[i].image;
-			}
+			people[i].val = diff;
+			alldiff.push(diff);
 		}
+		var lowdiff = Math.min(alldiff);
+		for(let j=0;j<people.length;j++){
+		if (lowdiff === people[j].val) {
 
-		
+			matchName = people[j].name;
+			matchImage = people[j].image;
+		}
+	};
+
 		people.push(thisuser);
 
-	
-		res.json({matchName: matchName, matchImage: matchImage});
+
+		res.json({ matchName: matchName, matchImage: matchImage });
 	});
 
 };
